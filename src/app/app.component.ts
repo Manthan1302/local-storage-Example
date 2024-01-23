6a34ad30d81fd2b00fffb342fd251663f2641896
Manthan Thakkar <Manthant@btl.com>
2024-01-23 14:19:59 +0530

    local Stroage with Angular Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'localStorageExample';
+  company:{name:string,gst:string}[]=[]
+  details:{name:string,gst:string}[]=[];
+  saveData(name:string,gst:string){
+    this.company.push({name:name,gst:gst})
+    localStorage.setItem('data',JSON.stringify(this.company))
+  }
+
+  loadData(){
+    let data:any = localStorage.getItem('data')
+    console.log(data);
+    this.details=JSON.parse(data)
+  }
 }
